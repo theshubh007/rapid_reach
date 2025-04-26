5354c6ece5be7cd2ef24e59904ede8581bc41d2d
shubham <thekingshubh07@gmail.com>
2025-04-26 12:03:33 -0700

    Init setup developed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Getting Started with Rapid Reach
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/components/MapComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+function MapComponent() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.5, -0.09]}>
+        <Popup>
+          A pretty CSS popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default MapComponent;
